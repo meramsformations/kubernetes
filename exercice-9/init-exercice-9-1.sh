66941b23e778cd803f6c9605a99853e497752b09
Student04 <student04.merams.formations@gmail.com>
2023-08-21 23:59:00 +0200

    kube exo 9

---
+++ exercice-9/init-exercice-9-1.sh
@@ -0,0 +1,4 @@
+
+kubectl apply -f ~/kubernetes/exercice-9/ubuntu-sleeper.yaml
+kubectl apply -f ~/kubernetes/exercice-9/ubuntu-sleeper-2.yaml
+kubectl apply -f ~/kubernetes/exercice-9/ubuntu-sleeper-3.yaml
