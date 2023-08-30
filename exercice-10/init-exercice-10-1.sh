55e96c332023f0353136f556b4b463db76c8fefa
Student04 <student04.merams.formations@gmail.com>
2023-08-30 15:34:34 +0200

    kube exo 10

--- exercice-10/init-exercice-10-1.sh
+++ exercice-10/init-exercice-10-1.sh
@@ -1,3 +1,4 @@
 
-kubectl create serviceaccount webapp-sa
+kubectl apply -f ~/kubernetes/exercice-10/webapp-sa-secret.yaml
+kubectl apply -f ~/kubernetes/exercice-10/webapp-sa.yaml
 kubectl apply -f ~/kubernetes/exercice-10/deployment-webapp.yaml
