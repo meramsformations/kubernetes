69e70098d2703a68f2a2e408ccb5be28c4be85a7
Ali Merams <merams.consulting@gmail.com>
2023-10-04 00:44:08 +0200

    add exo 14

---
+++ exercice-14/curl-test.sh
@@ -0,0 +1,6 @@
+for i in {1..20}; do
+   kubectl exec curl -- sh -c 'test=`wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/ready 2>&1` && echo "$test OK" || echo "Failed"';
+   echo ""
+done
+
+
---
+++ exercice-14/init-exercice-14-1.sh
@@ -0,0 +1,4 @@
+
+kubectl apply -f ~/kubernetes/exercice-14/curl.yaml
+kubectl apply -f ~/kubernetes/exercice-14/simple-webapp-1.yaml
+kubectl apply -f ~/kubernetes/exercice-14/webapp-service.yaml
