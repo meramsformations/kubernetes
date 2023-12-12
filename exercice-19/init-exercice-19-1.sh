4a8dbde04f39495e37e7a2d24815edaf11d1aa52
Ali Merams <merams.consulting@gmail.com>
2023-12-13 00:40:13 +0100

    exo 19

---
+++ exercice-19/init-exercice-19-1.sh
@@ -0,0 +1,14 @@
+#Ajouter la configuration Docker Hub avec le compte pro de esilv
+kubectl get secret docker-hub-pull-secret -n default -o name > /dev/null 2>/dev/null
+if [ $? -ne 0 ]
+then
+ kubectl create secret docker-registry docker-hub-pull-secret --docker-server="https://index.docker.io/v1/" --docker-username=crdevinci --docker-password=dckr_pat_YfO7bNq4XbdFL0hcZlpm7YjUEiU -n default
+fi
+
+kubectl get sa default -n default -o yaml 2>/dev/null | grep -i docker-hub-pull-secret >/dev/null 2>/dev/null
+if [ $? -ne 0 ]
+then
+  kubectl patch serviceaccount default -p '{"imagePullSecrets": [{"name": "docker-hub-pull-secret"}]}' -n default
+fi
+
+kubectl apply -f ~/kubernetes/exercice-19/curl.yaml
